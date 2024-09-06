0d7af387e390ae1c4c41581bfbd3fd412184b129
ViniciusMarcuS2i <marcusononato@gmail.com>
2024-09-06 10:49:54 -0400

    feat: add search component

---
+++ app/_components/search.tsx
@@ -0,0 +1,17 @@
+import { SearchIcon } from "lucide-react";
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
+export function Search() {
+  return (
+    <div className="flex gap-2">
+      <Input
+        placeholder="Buscar restaurantes"
+        className="border-none bg-[#d7d7d7]"
+      />
+      <Button size="icon">
+        <SearchIcon size={20} />
+      </Button>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
 import { Header } from "./_components/header";
+import { Search } from "./_components/search";
+import { Input } from "./_components/ui/input";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div className="px-5 pt-6">
+        <Search />
+      </div>
+    </>
+  );
 }
