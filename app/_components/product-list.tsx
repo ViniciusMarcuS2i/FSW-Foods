cad4ba82bd7eaee5329d7446dc64c341837354f5
ViniciusMarcuS2i <marcusononato@gmail.com>
2024-10-22 12:18:40 -0400

    refactor: ensure product list receibes products

--- app/_components/product-list.tsx
+++ app/_components/product-list.tsx
@@ -1,23 +1,20 @@
+import { Prisma } from "@prisma/client";
 import { db } from "../_lib/prisma";
 import { ProductItem } from "./product-item";
 
-export async function ProductList() {
-  const products = await db.product.findMany({
-    where: {
-      discountPercentage: {
-        gt: 0,
-      },
-    },
-    take: 10,
+interface PruductListProps {
+  products: Prisma.ProductGetPayload<{
     include: {
       restaurant: {
         select: {
-          name: true,
-        },
-      },
-    },
-  });
+          name: true;
+        };
+      };
+    };
+  }>[];
+}
 
+export async function ProductList({ products }: PruductListProps) {
   return (
     <div className="flex gap-6 overflow-x-scroll px-5 [&::-webkit-scrollbar]:hidden">
       {products.map((products) => (
