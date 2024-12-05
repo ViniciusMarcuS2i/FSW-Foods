e93417af36d946cd1d3b1e7aaea7c19008374766
ViniciusMarcuS2i <marcusononato@gmail.com>
2024-12-05 16:39:48 -0400

    fix: fixed favorited restaurants page

--- app/my-favorites-restaurants/page.tsx
+++ app/my-favorites-restaurants/page.tsx
@@ -1,18 +1,18 @@
-import { getServerSession } from "next-auth";
+import Header from "../_components/header";
+import RestaurantItem from "../_components/restaurant-item";
 import { db } from "../_lib/prisma";
 import { authOptions } from "../_lib/auth";
+import { getServerSession } from "next-auth";
 import { notFound } from "next/navigation";
-import { Header } from "../_components/header";
-import { RestaurantItem } from "../_components/restaurant-item";
 
-const MyFavoriteRestaurantsPage = async () => {
+const MyFavoritesRestaurants = async () => {
   const session = await getServerSession(authOptions);
 
   if (!session) {
     return notFound();
   }
 
-  const userFavoriteRestaurants = await db.userFavoriteResttaurant.findMany({
+  const userFavoritesRestaurants = await db.userFavoriteResttaurant.findMany({
     where: {
       userId: session?.user.id,
     },
@@ -23,27 +23,28 @@ const MyFavoriteRestaurantsPage = async () => {
   return (
     <>
       <Header />
-      <div className="px-5 py-6">
-        <h2 className="mb-6 text-lg font-semibold">Restaurantes Favoritos</h2>
-        {userFavoriteRestaurants.length > 0 ? (
-          <>
-            <div className="flex flex-col gap-6">
-              {userFavoriteRestaurants.map(({ restaurant }) => (
-                <RestaurantItem
-                  key={restaurant.id}
-                  restaurant={restaurant}
-                  className="min-w-full max-w-full"
-                  userFavoriteRestaurants={userFavoriteRestaurants}
-                />
-              ))}
-            </div>
-          </>
-        ) : (
-          <h2>Você ainda não favoritou nenhum restaurante.</h2>
-        )}
+
+      <div className="mx-auto max-w-[1224px] py-6 max-xl:px-5">
+        <h2 className="mb-6 text-lg font-semibold">Restaurantes favoritos</h2>
+        <div className="grid w-full grid-cols-3 gap-6 max-lg:flex max-lg:flex-col">
+          {userFavoritesRestaurants.length > 0 ? (
+            userFavoritesRestaurants.map(({ restaurant }) => (
+              <RestaurantItem
+                className="min-w-full max-w-full"
+                key={restaurant.id}
+                restaurant={restaurant}
+                userFavoriteRestaurants={userFavoritesRestaurants}
+              />
+            ))
+          ) : (
+            <h3 className="text-left font-medium text-muted-foreground">
+              Você ainda não adicionou nenhum restaurante aos favoritos.
+            </h3>
+          )}
+        </div>
       </div>
     </>
   );
 };
 
-export default MyFavoriteRestaurantsPage;
+export default MyFavoritesRestaurants;
